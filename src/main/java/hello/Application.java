0275a912cc9e91ed6ed8b8cbd8721ae53b2e6139
Aflynch <aflynch@gmail.com>
2016-09-19 12:30:26 -0400

    First  Commit for Shaun's project
    Hello World

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Aflynch on 9/18/2016.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
